46431db211392cf6d5e95c710ec66d1ac8f36d7f
ayujain19 <ayushdagda19@gmail.com>
2021-03-29 19:36:15 +0530

    Initial Commit

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 const path = require('path');
 
 const staticPath = path.join(__dirname,"../public");
